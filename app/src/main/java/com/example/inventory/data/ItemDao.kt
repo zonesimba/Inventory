8e0f58075855244dc62e9ba30a2844d5ce3c4dc1
kakudji simba gentil <21ks128@esisalama.org>
2024-05-31 10:20:33 +0200

    ajout d'elements et fonction dans InventoryDatabase

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,6 +1,22 @@
 package com.example.inventory.data
+
+import androidx.contentpager.content.Query
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface ItemDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * from items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * from items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
 }
